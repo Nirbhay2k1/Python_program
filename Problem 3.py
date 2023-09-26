bd14229a59eca5a4a55db99b00ff3c861b47f4a3
Nirbhay Agarwal <98315109+Nirbhay2k1@users.noreply.github.com>
2023-09-26 16:41:21 +0530

    Create Problem 3.py

---
+++ Problem 3.py
@@ -0,0 +1,12 @@
+def fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+nterms = 10
+
+
+print("Fibonacci sequence:")
+for i in range(nterms):
+    print(fibo(i))
